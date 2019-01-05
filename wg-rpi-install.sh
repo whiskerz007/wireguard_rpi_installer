7861a153f9b3b4f869862088967d537b9d4e10eb
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2019-01-05 20:24:00 +0100

    Fixed array

--- wg-rpi-install.sh
+++ wg-rpi-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+###############################################################
+# Created by whiskerz007                                      #
+# URL: https://github.com/whiskerz007/wireguard_rpi_installer #
+###############################################################
 function install() {
     apt-get update \
     && apt-get upgrade -y \
@@ -9,6 +12,8 @@ function install() {
     && printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' | tee --append /etc/apt/preferences.d/limit-unstable \
     && apt-get update \
     && apt-get install -y wireguard \
+    # && sysctl -w net.ipv4.ip_forward=1 \
+    # Store "sysctl net.ipv4.ip_forward" setting for permenant change
     && modprobe wireguard \
     && exit 0 \
     || (echo "ERROR: Failed to complete installtion." && exit 1)
@@ -23,14 +28,14 @@ if [[ $EUID -ne 0 ]]; then
     && exit 1
 fi
 
-declare -r -a SUPPORTED_BOARDS=( \
-    "a22042" \ # 2 Model B (with BCM2837) v1.2 1GB
-    "a02082" \ # 3 Model B v1.2	1GB
-    "a020a0" \ # Compute Module 3 (and CM3 Lite) v1.0 1GB
-    "a22082" \ # 3 Model B v1.2 1GB
-    "a32082" \ # 3 Model B v1.2 1GB
-    "a020d3" \ # 3 Model B+ v1.3 1GB
-    "9020e0" \ # 3 Model A+ v1.0 512MB
+declare -r -a SUPPORTED_BOARDS=(
+    "a22042" # 2 Model B (with BCM2837) v1.2 1GB
+    "a02082" # 3 Model B v1.2	1GB
+    "a020a0" # Compute Module 3 (and CM3 Lite) v1.0 1GB
+    "a22082" # 3 Model B v1.2 1GB
+    "a32082" # 3 Model B v1.2 1GB
+    "a020d3" # 3 Model B+ v1.3 1GB
+    "9020e0" # 3 Model A+ v1.0 512MB
     )
 declare -r BOARD=`awk '/^Revision/ {sub("^1000", "", $3); print $3}' /proc/cpuinfo`
 
@@ -40,6 +45,6 @@ for i in ${!SUPPORTED_BOARDS}; do
     fi
 done
 echo -e "ERROR: Unsupported device detected!\n" \
-    "Refer to the following website for installation instructions.\n" \
-    "https://www.wireguard.com/install/\n" \
+        "Refer to the following website for installation instructions.\n" \
+        "https://www.wireguard.com/install/\n" \
 && exit 1
