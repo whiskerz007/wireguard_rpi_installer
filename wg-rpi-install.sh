cf9771a9b0999d449ec5a607774d7a1b8703fe1f
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2019-01-06 00:03:34 +0100

    Added support for compiling

--- wg-rpi-install.sh
+++ wg-rpi-install.sh
@@ -3,7 +3,7 @@
 # Created by whiskerz007                                      #
 # URL: https://github.com/whiskerz007/wireguard_rpi_installer #
 ###############################################################
-function install() {
+function install_deb() {
     apt-get update \
     && apt-get upgrade -y \
     && apt-get install -y raspberrypi-kernel-headers dirmngr \
@@ -12,16 +12,34 @@ function install() {
     && printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' | tee --append /etc/apt/preferences.d/limit-unstable 1>/dev/null \
     && apt-get update \
     && apt-get install -y wireguard \
-    && echo "wireguard" | tee --append /etc/modules 1>/dev/null \
-    && modprobe wireguard \
-    && sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf \
+    || failed
+}
+function install_make() {
+    apt-get update \
+    && apt-get upgrade -y \
+    && apt-get install -y raspberrypi-kernel-headers libmnl-dev libelf-dev build-essential git \
+    && git clone https://git.zx2c4.com/WireGuard /tmp/wireguard \
+    && cd /tmp/wireguard \
+    && make \
+    && make install \
+    && finish \
+    || failed
+}
+function finish() {
+    # echo "wireguard" | tee --append /etc/modules 1>/dev/null \
+    # && modprobe wireguard \
+    sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf \
     && sysctl -p \
     && echo -e "\n\n" \
         "#######################################\n" \
         "# WireGuard installation is complete! #\n" \
         "#######################################\n" \
     && exit 0 \
-    || (echo "ERROR: Failed to complete installation." && exit 1)
+    || failed
+}
+function failed() {
+    echo "ERROR: Failed to complete installation." \
+    && exit 1
 }
 
 if [[ $EUID -ne 0 ]]; then
@@ -29,20 +47,37 @@ if [[ $EUID -ne 0 ]]; then
     && exit 1
 fi
 
-declare -r -a SUPPORTED_BOARDS=(
+# Board model infomation pulled from: https://elinux.org/RPi_HardwareHistory
+declare -r -a SUPPORTED_BOARDS_DEB=(
     "a22042" # 2 Model B (with BCM2837) v1.2 1GB
     "a02082" # 3 Model B v1.2 1GB
     "a020a0" # Compute Module 3 (and CM3 Lite) v1.0 1GB
-    "a22082" # 3 Model B v1.2 1GB
-    "a32082" # 3 Model B v1.2 1GB
+    "a22082" # 3 Model B v1.2 1GB (Mfg by Embest)
+    "a32082" # 3 Model B v1.2 1GB (Mfg by Sony Japan)
     "a020d3" # 3 Model B+ v1.3 1GB
     "9020e0" # 3 Model A+ v1.0 512MB
-    )
+)
+declare -r -a SUPPORTED_BOARDS_MAKE=(
+    "900021" # A+ v1.1 512MB
+    "900032" # B+ v1.2 512MB
+    "900092" # Zero v1.2 512MB
+    "900093" # Zero v1.3 512MB (Mfg by Sony)
+    "920093" # Zero v1.3 512MB (Mfg by Embest)
+    "9000c1" # Zero W v1.1 512MB
+    "a01040" # 2 Model B v1.0 1GB
+    "a01041" # 2 Model B v1.1 1GB (Mfg by Sony)
+    "a21041" # 2 Model B v1.1 1GB (Mfg by Embest)
+)
 declare -r BOARD=`awk '/^Revision/ {sub("^1000", "", $3); print $3}' /proc/cpuinfo`
 
-for i in "${SUPPORTED_BOARDS[@]}"; do
+for i in "${SUPPORTED_BOARDS_DEB[@]}"; do
     if [[ ${i} == ${BOARD} ]]; then
-        install
+        install_deb
+    fi
+done
+for i in "${SUPPORTED_BOARDS_MAKE[@]}"; do
+    if [[ ${i} == ${BOARD} ]]; then
+        install_make
     fi
 done
 echo -e "ERROR: Unsupported device detected!\n" \
